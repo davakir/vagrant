610805050724899ecf35087b8ce61785ce0adac0
Darya Kiryanova <bella21abyss@gmail.com>
2016-11-05 10:57:18 +0300

    fix typo

--- setup/postgresql.sh
+++ setup/postgresql.sh
@@ -56,7 +56,7 @@ echo "PGROOT=/var/lib/postgresql" >> ~/.bashrc
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
 
 # Append to pg_hba.conf to add password auth:
-echo "host    all             all             all                     md5" > "$PG_HBA"
+echo "host    all             all             all                     md5" >> "$PG_HBA"
 
 # Explicitly set default client_encoding
 echo "client_encoding = utf8" >> "$PG_CONF"
